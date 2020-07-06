d23d6875b90bfd2f828c9d594b8638e8fa83eecd
schmiedm <61515794+schmiedm@users.noreply.github.com>
2020-07-06 12:36:37 -0400

    Added Calendar

---
+++ Controllers/CalendarController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BurlOakMovers.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Calendar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEvents()
+        {
+            using (Entities2 dc = new Entities2())
+            {
+                var events = dc.TestEvents.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+    }
+}
