a19db6516ebfe3949f78e5e879f5e931df11c262
schmiedm <61515794+schmiedm@users.noreply.github.com>
2020-08-05 10:52:06 -0400

    Addedd update calendar functionality Still borken it is a work in progress

--- Controllers/CalendarController.cs
+++ Controllers/CalendarController.cs
@@ -22,5 +22,52 @@ namespace BurlOakMovers.Controllers
                 return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
             }
         }
+        [HttpPost]
+        public JsonResult SaveEvent(TestEvent e)
+        {
+            var status = false;
+            using (Entities2 dc = new Entities2())
+            {
+                if (e.id > 0)
+                {
+                    var v = dc.TestEvents.Where(a => a.id == e.id).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.IsFullDay = e.IsFullDay;
+                        v.Start = e.Start;
+                        v.Subject = e.Subject;
+                        v.Description = e.Description;
+                        v.ThemeColor = e.ThemeColor;
+                        v.End = e.End;
+                    }
+                }
+                else
+                {
+                    dc.TestEvents.Add(e);
+                }
+                dc.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpPost]
+        public JsonResult DeleteEvent(int id)
+        {
+            var status = false;
+
+            using (Entities2 dc = new Entities2())
+            {
+                var v = dc.TestEvents.Where(a => a.id == id).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.TestEvents.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+
+                return new JsonResult { Data = new { status = status } };
+        }
     }
 }
